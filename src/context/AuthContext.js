636abe0f2ecab4db080bff9f9537518fd341f235
Ozkan Yilmaz <ozkanyilmaz@gmail.com>
2019-10-26 21:30:28 +0300

    add signup screen api call

---
+++ src/context/AuthContext.js
@@ -0,0 +1,34 @@
+import createDataContext from "./createDataContext";
+import trackerApi from "../api/tracker";
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const signup = dispatch => {
+  return async ({ email, password }) => {
+    try {
+      const response = await trackerApi.post("/signup", { email, password });
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+const signin = dispatch => {
+  return ({ email, password }) => {};
+};
+
+const signout = dispatch => {
+  return () => {};
+};
+
+export const { Provider, Context } = createDataContext(
+  authReducer,
+  { signin, signup, signout },
+  { isSignedIn: false }
+);
