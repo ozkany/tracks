e5bb561066d148b4a2faf6e72d88a5b735303c5c
Ozkan Yilmaz <ozkanyilmaz@gmail.com>
2019-11-06 21:21:42 +0300

    Add NavigationEvents onWillBlur

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,38 +7,47 @@ const authReducer = (state, action) => {
   switch (action.type) {
     case "add_error":
       return { ...state, errorMessage: action.payload };
-    case "signup":
+    case "signin":
       return { errorMessage: "", token: action.payload };
+    case "clear_error_message":
+      return { ...state, errorMessage: "" };
     default:
       return state;
   }
+};
+
+const clearErrorMessage = dispatch => () => {
+  dispatch({ type: "clear_error_message" });
 };
 
 const signup = dispatch => async ({ email, password }) => {
   try {
     const response = await trackerApi.post("/signup", { email, password });
     await AsyncStorage.setItem("token", response.data.token);
-
-    console.log(response);
-
-    dispatch({ type: "signup", payload: response.data.token });
-
-    console.log("dispacthed signup");
-
+    dispatch({ type: "signin", payload: response.data.token });
     navigate("TrackList");
-
-    console.log("navigated to tracklist");
   } catch (err) {
     console.log(err);
     dispatch({
       type: "add_error",
-      payload: "Something went wrong with signup"
+      payload: "Something went wrong with sign up"
     });
   }
 };
 
-const signin = dispatch => {
-  return ({ email, password }) => {};
+const signin = dispatch => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post("/signin", { email, password });
+    await AsyncStorage.setItem("token", response.data.token);
+    dispatch({ type: "signin", payload: response.data.token });
+    navigate("TrackList");
+  } catch (err) {
+    console.log(err);
+    dispatch({
+      type: "add_error",
+      payload: "Something went wrong with sign in"
+    });
+  }
 };
 
 const signout = dispatch => {
@@ -47,6 +56,6 @@ const signout = dispatch => {
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  { signin, signup, signout },
+  { signin, signup, signout, clearErrorMessage },
   { token: null, errorMessage: "" }
 );
